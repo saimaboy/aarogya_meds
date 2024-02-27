69ae66dd0e97e236615aed8e274df2e6e28498ed
avishka2k <avishka2k@gmail.com>
2024-02-27 21:36:59 +0530

    common dart

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,1 +1,16 @@
 //main screen
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatefulWidget {
+  const Wrapper({super.key});
+
+  @override
+  State<Wrapper> createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
